e7287afaa8929e16100f67e6eeb1ca19ee3bd3f1
lucasdgimenez <lucas_d360@hotmail.com>
2023-03-07 19:18:14 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Task;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class
+        ]);
+        
+        Category::factory(5)->create();
+        Task::factory(30)->create();
+    }
+}
